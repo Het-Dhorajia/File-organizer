8ac04578caf8a8a3550fce2e44e631c29df60003
Het-Dhorajia <hetdhorajia21@gmail.com>
2026-05-27 15:48:00 +0530

    working

--- file.py
+++ file.py
@@ -4,7 +4,22 @@ folder_path = input("Enter folder path: ")
 
 files = os.listdir(folder_path)
 
-print("\nFiles found:\n")
-
 for file in files:
-    print(file)
+    name, ext = os.path.splitext(file)
+
+    ext = ext.lower()
+
+    if ext in [".jpg", ".png", ".jpeg"]:
+        print(file, "→ Image")
+
+    elif ext in [".mp4", ".mkv", ".avi"]:
+        print(file, "→ Video")
+
+    elif ext in [".pdf", ".docx", ".txt"]:
+        print(file, "→ Document")
+
+    elif ext in [".zip", ".rar"]:
+        print(file, "→ Archive")
+
+    else:
+        print(file, "→ Other")
